5cddc79a6290bb1478b1bc38d56abda09c4a0e69
AnthonyKazyaka <ask.anthony@hotmail.com>
2014-07-22 23:29:14 -0400

    Initial commit.

---
+++ Assets/Scripts/Gear Generation Scripts/RarityGenerator.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+public class RarityGenerator : MonoBehaviour
+{
+    public enum WeaponRarity
+    {
+        Common,
+        Special,
+        Rare,
+        UltraRare,
+        Legendary
+    }
+
+    public bool generateNewRarity = false;
+    public int numberOfNewItems = 100;
+
+    //public List<WeaponRarity> Rarities = new List<WeaponRarity>();
+
+    public int numberOfCommon = 0;
+    public int numberOfSpecial = 0;
+    public int numberOfRare = 0;
+    public int numberOfUltraRare = 0;
+    public int numberOfLegendary = 0;
+
+	// Use this for initialization
+	void Start () {
+	    GenerateNewRarities(numberOfNewItems);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	    if (generateNewRarity)
+	    {
+	        generateNewRarity = false;
+            GenerateNewRarities(numberOfNewItems);
+	    }
+	}
+
+    public void GenerateNewRarities(int numberofItems)
+    {
+        for (int i = 0; i < numberofItems; i++)
+        {
+            int random = (int) (1000 * Random.value);
+
+            if (random <= 500)
+            {
+                numberOfCommon++;
+            }
+            else if (random > 600 && random <= 900)
+            {
+                numberOfSpecial++;
+            }
+            else if (random > 900 && random <= 975)
+            {
+                numberOfRare++;
+            }
+            else if (random > 975 && random <= 995)
+            {
+                numberOfUltraRare++;
+            }
+            else if(random > 995 && random <=1000)
+            {
+                numberOfLegendary++;
+            }
+
+
+
+            //int rand1 = (int) (Random.value * 10);
+            //int rand2 = (int) (Random.value * 10);
+            //int rand3 = (int)(Random.value * 10);   
+        }
+
+    }
+}
---
+++ Assets/Scripts/Player Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private float _movementSpeed = 5.0f;
+    public float MovementSpeed
+    {
+        get { return _movementSpeed; }
+        private set { _movementSpeed = value; }
+    }
+
+    private float _minimumThumbstickThreshold = 0.05f;
+    
+    // Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+    private void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movementVector = Vector3.zero;
+        if (Mathf.Abs(Xbox360Controller.Player1Contoller.LeftThumbstickX) > _minimumThumbstickThreshold)
+        {
+            movementVector += Xbox360Controller.Player1Contoller.LeftThumbstickX * gameObject.transform.right;
+            Debug.Log("Player 1 X!");
+        }
+        if (Mathf.Abs(Xbox360Controller.Player1Contoller.LeftThumbstickY) > _minimumThumbstickThreshold)
+        {
+            movementVector += Xbox360Controller.Player1Contoller.LeftThumbstickY * gameObject.transform.forward;
+            Debug.Log("Player 1 Y!");
+        }
+
+        movementVector = gameObject.transform.position + (movementVector * MovementSpeed * Time.deltaTime);
+
+        Debug.Log(movementVector);
+        gameObject.rigidbody.MovePosition(movementVector);
+    }
+}
